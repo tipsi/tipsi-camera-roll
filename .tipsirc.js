c1995724096d7bbefc62928ef78565ed40cc0bb1
Anton Kuznetsov <isnifer@gmail.com>
2018-09-07 15:24:39 +0300

    Update tipsi-travis-scripts

--- .tipsirc.js
+++ .tipsirc.js
@@ -1,4 +1,21 @@
 export default {
+  // Default value — []
+  filesToCopy: [
+    '.appiumhelperrc',
+    'package.json',
+    'android/build.gradle',
+    'android/app/build.gradle',
+    'android/gradle/wrapper/gradle-wrapper.properties',
+    'android/gradle.properties',
+    'ios/example/AppDelegate.m',
+    'src',
+    'scripts',
+    '__tests__',
+    'rn-cli.config.js',
+    'App.js',
+    'ios/Podfile',
+  ],
+
   // Default value — { ios: '', android: '' }
   test: {
     ios: 'npm run test:ios',
